804bc84fbff850aebbf30b9586d6662fdb197f71
jetsetr999 <31719002+jetsetr999@users.noreply.github.com>
2025-04-25 19:22:40 -0600

    wk1 part2 assignment complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //get user input declaring grade percentage
+        Console.Write("What is your grade percentage? ");
+        string ui = Console.ReadLine();
+        int gradePercentage = int.Parse(ui);
+
+        string grade;
+
+        if(gradePercentage >= 90)
+        {
+            grade = "A";
+        }
+        else if(gradePercentage >= 80)
+        {
+            grade = "B";
+        }
+        else if(gradePercentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradePercentage >= 60)
+       {
+            grade = "D";
+       }
+       else 
+       {
+            grade = "F";
+       }
+
+       Console.WriteLine($"Your grade is {grade} ");
+    
+        //if (letter == "A" || letter == "B" || letter == "C")
+        if (gradePercentage >= 70)
+        {
+        Console.WriteLine(" ----- Congratulations you passed ----- ");
+        }
+        else
+        {
+        Console.WriteLine("--- Sorry you failed seek help if needed and try again ---");
+        }
+
     }
 }
