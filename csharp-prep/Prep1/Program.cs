4133aabe46bbe3a11cd981c73380396de69b6a84
jetsetr999 <31719002+jetsetr999@users.noreply.github.com>
2025-04-25 19:21:46 -0600

    wk1 part 1 assignment complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // get user input
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+                
+        // display user input
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
