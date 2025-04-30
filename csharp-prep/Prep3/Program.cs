9963c26b9df0319338bd714fbaf1009c055eaa32
jetsetr999 <31719002+jetsetr999@users.noreply.github.com>
2025-04-30 00:58:08 -0600

    week 2 prep assignments 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Net.NetworkInformation;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //1-DisplayWelcome - Displays the message, "Welcome to the Program!"
+        DisplayMessage();
+
+        //2-PromptUserName - Asks for and returns the user's name (as a string)
+        string userName = PromptName();
+
+        //3-PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+        int userNumber = PromptNumber();
+
+        //4-SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+        int SquaredNumber = SqNumber(userNumber);
+
+        //5-DisplayResult - Accepts the user's name and the squared number and displays them.
+        DisplayResults(userName, SquaredNumber);
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        static int SqNumber(int number)
+        {
+            int square = number * number;
+
+            return square;
+        }
+
+        static void DisplayResults(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }        
     }
 }
