fbedf69c4c5c078ccc6528287a1c1118a440e249
jetsetr999 <31719002+jetsetr999@users.noreply.github.com>
2025-05-05 22:19:48 -0600

    week 3 learning classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        //job1.DisplayJobs();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        //job2.DisplayJobs();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobList.Add(job1);
+        myResume._jobList.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
